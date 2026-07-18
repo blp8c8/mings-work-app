80cd934647a3be55fee14495e3f33f28176aa5aa
blp8c8 <152976233+blp8c8@users.noreply.github.com>
2026-07-18 22:14:33 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(
+      process.env.SUPABASE_URL || process.env.VITE_SUPABASE_URL || ''
+    ),
+    'import.meta.env.VITE_SUPABASE_KEY': JSON.stringify(
+      process.env.SUPABASE_ANON_KEY || process.env.VITE_SUPABASE_KEY || ''
+    ),
+  }
 })
-
